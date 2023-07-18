4eaec001732210fbcc04bc3b07c7ac65c7340b25
Harishsnow <139842948+Harishsnow@users.noreply.github.com>
2023-07-18 17:28:18 +0530

    Update stramlit_app.py

--- stramlit_app.py
+++ stramlit_app.py
@@ -22,3 +22,7 @@ import requests
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
 streamlit.text(fruityvice_response)
 
+streamlit.header('Fruitvice Fruit Advice!')
+import requests 
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
+streamlit.text(fruityvice_response.json())
