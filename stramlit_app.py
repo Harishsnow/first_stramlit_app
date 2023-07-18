b63ef905a44a30b19b4d7074bf7314d488692de9
Harishsnow <139842948+Harishsnow@users.noreply.github.com>
2023-07-18 17:32:09 +0530

    Update stramlit_app.py

--- stramlit_app.py
+++ stramlit_app.py
@@ -24,8 +24,8 @@ streamlit.text(fruityvice_response)
 
 streamlit.header('Fruitvice Fruit Advice!')
 import requests 
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
-streamlit.text(fruityvice_response.json())
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + "Kiwi")
+
 
 
 # write your own comment -what does the next line do? 
